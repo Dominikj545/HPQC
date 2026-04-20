76cc4af13e98f811222aeda3f2b98d214ae60497
Dominik Jakobczak <dominikj545@gmail.com>
2026-04-20 19:39:35 +0100

    functionalised the client task

--- week4/comm_test_mpi.c
+++ week4/comm_test_mpi.c
@@ -2,6 +2,7 @@
 #include <mpi.h>
 
 void root_task(int my_rank, int uni_size);
+void client_task(int my_rank, int uni_size);
 
 int main(int argc, char **argv)
 {
@@ -13,10 +14,6 @@ int main(int argc, char **argv)
 	ierror = MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	ierror = MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
 
-	int send_message, count, dest, tag;
-	send_message = dest = tag = 0;
-	count = 1;
-
 	if (uni_size > 1)
 	{
 		if (0 == my_rank)
@@ -25,11 +22,7 @@ int main(int argc, char **argv)
 		} // end if (0 == my_rank)
 		else
 		{
-			dest = 0;
-			send_message = my_rank * 10;
-			MPI_Send(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD);
-			printf("Hello, I am %d of %d. Sent %d to Rank %d\n",
-					my_rank, uni_size, send_message, dest);
+			client_task(my_rank, uni_size);
 		} // end else
 	} // end if (uni_size > 1)
 	else
@@ -54,4 +47,17 @@ void root_task(int my_rank, int uni_size)
 		printf("Hello, I am %d of %d. Received %d from Rank %d\n",
 				my_rank, uni_size, recv_message, source);
 	} // end for (source = 1; source < uni_size; source++)
+}
+
+void client_task(int my_rank, int uni_size)
+{
+	int send_message, count, dest, tag;
+	send_message = dest = tag = 0;
+	count = 1;
+
+	dest = 0;
+	send_message = my_rank * 10;
+	MPI_Send(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD);
+	printf("Hello, I am %d of %d. Sent %d to Rank %d\n",
+			my_rank, uni_size, send_message, dest);
 }
