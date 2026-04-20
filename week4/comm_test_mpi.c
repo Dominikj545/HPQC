d205a4119ee5f40533195d1c5865aa7b3b93f406
Dominik Jakobczak <dominikj545@gmail.com>
2026-04-20 19:35:37 +0100

    functionalised the root task

--- week4/comm_test_mpi.c
+++ week4/comm_test_mpi.c
@@ -1,70 +1,57 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char **argv) 
+void root_task(int my_rank, int uni_size);
+
+int main(int argc, char **argv)
 {
-	// declare and initialise error handling variable
 	int ierror = 0;
-	
-	// declare and initialise rank and size varibles
+
 	int my_rank, uni_size;
 	my_rank = uni_size = 0;
-
-	// intitalise MPI
 	ierror = MPI_Init(&argc, &argv);
+	ierror = MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	ierror = MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
 
-	// gets the rank and world size
-	ierror = MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-	ierror = MPI_Comm_size(MPI_COMM_WORLD,&uni_size);
-
-	// creates and initialies transmission variables
-	int send_message, recv_message, count, dest, source, tag;
-	send_message = recv_message = dest = source = tag = 0;
+	int send_message, count, dest, tag;
+	send_message = dest = tag = 0;
 	count = 1;
-	MPI_Status status;
-	
+
 	if (uni_size > 1)
 	{
 		if (0 == my_rank)
 		{
-			// iterates through all the other ranks
-			for (int their_rank = 1; their_rank < uni_size; their_rank++)
-			{
-				// sets the source argument to the rank of the sender
-				source = their_rank;
-
-				// receives the messages
-				MPI_Recv(&recv_message, count, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
-
-				// prints the message from the sender
-				printf("Hello, I am %d of %d. Received %d from Rank %d\n",
-						my_rank, uni_size, recv_message, source);
-			} // end for (int their_rank = 1; their_rank < uni_size; their_rank++)
+			root_task(my_rank, uni_size);
 		} // end if (0 == my_rank)
-		else // i.e. (0 != my_rank)
+		else
 		{
-			// sets the destination for the message
-			dest = 0; // destination is root
-
-			// creates the message
+			dest = 0;
 			send_message = my_rank * 10;
-
-			// sends the message
 			MPI_Send(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD);
-
-			// prints the message from the sender
-                        printf("Hello, I am %d of %d. Sent %d to Rank %d\n",
-                                         my_rank, uni_size, send_message, dest);
-
-		} // end else // i.e. (0 != my_rank)
+			printf("Hello, I am %d of %d. Sent %d to Rank %d\n",
+					my_rank, uni_size, send_message, dest);
+		} // end else
 	} // end if (uni_size > 1)
-	else // i.e. uni_size <=1
+	else
 	{
-		// prints a warning
 		printf("Unable to communicate with less than 2 processes. MPI communicator size = %d\n", uni_size);
 	}
 
-	// finalise MPI
 	ierror = MPI_Finalize();
 	return 0;
+}
+
+void root_task(int my_rank, int uni_size)
+{
+	int recv_message, count, source, tag;
+	recv_message = source = tag = 0;
+	count = 1;
+	MPI_Status status;
+
+	for (source = 1; source < uni_size; source++)
+	{
+		MPI_Recv(&recv_message, count, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+		printf("Hello, I am %d of %d. Received %d from Rank %d\n",
+				my_rank, uni_size, recv_message, source);
+	} // end for (source = 1; source < uni_size; source++)
 }
